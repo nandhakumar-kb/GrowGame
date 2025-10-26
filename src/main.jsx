67cc5d37b1dad20fa639d507c46071626bb5e542
nandhakumar-kb <nandhakumarkb2005@gmail.com>
2025-10-26 21:49:54 +0530

    Update PWA configuration and service worker

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { register as registerServiceWorker, setupInstallPrompt } from './utils/pwaUtils'
-registerServiceWorker();
-setupInstallPrompt();
+
+// Only register service worker in production
+if (import.meta.env.PROD) {
+  registerServiceWorker();
+  setupInstallPrompt();
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
